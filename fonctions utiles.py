c0fa80dc1cd76fe501bf71823679d9775d137d95
manpct <manonpicot@orange.fr>
2025-03-10 16:56:13 +0100

    j'essaye de trensformer le mot en astrixe

---
+++ fonctions utiles.py
@@ -0,0 +1,20 @@
+import random as rd
+import tkinter as tk 
+
+racine = tk.Tk()
+def choix_mot(dico):
+    global mot
+    mot = rd.choice(dico)
+    return mot 
+
+def mot_en_asterix(word):
+    dico={}
+    word_l=list(word)
+    print(word_l)
+    for i in range (len(word_l)) :
+        dico[str(word_l[i])]='*'
+    return dico
+print(mot_en_asterix("manon"))
+dico = mot_en_asterix("manon")
+
+
