c2f7bf539898a5d25015e6f671b5d2f3c54f765a
manon123-design <manon.pinar@gmail.com>
2025-03-10 16:10:24 +0100

    lettre

--- Lettres.py
+++ Lettres.py
@@ -8,42 +8,56 @@ def affichage():
 
 bouton_a = tk.Button(racine,text='A',command=affichage)
 bouton_a.grid(column=0, row=0)
+
 bouton_b = tk.Button(racine,text='B',command=affichage)
 bouton_b.grid(column=1, row=0)
+
 bouton_c = tk.Button(racine,text='C',command=affichage)
 bouton_c.grid(column=2, row=0)
-bouton_a = tk.Button(racine,text='D',command=affichage)
-bouton_a.grid(column=3, row=0)
-bouton_a = tk.Button(racine,text='E',command=affichage)
-bouton_a.grid(column=4, row=0)
-bouton_a = tk.Button(racine,text='F',command=affichage)
-bouton_a.grid(column=5, row=0)
-bouton_a = tk.Button(racine,text='G',command=affichage)
-bouton_a.grid(column=6, row=0)
-bouton_a = tk.Button(racine,text='H',command=affichage)
-bouton_a.grid(column=7, row=0)
-bouton_a = tk.Button(racine,text='I',command=affichage)
-bouton_a.grid(column=8, row=0)
-bouton_a = tk.Button(racine,text='J',command=affichage)
-bouton_a.grid(column=9, row=0)
-bouton_a = tk.Button(racine,text='K',command=affichage)
-bouton_a.grid(column=10, row=0)
-bouton_a = tk.Button(racine,text='L',command=affichage)
-bouton_a.grid(column=11, row=0)
+
+bouton_d = tk.Button(racine,text='D',command=affichage)
+bouton_d.grid(column=3, row=0)
+
+bouton_e = tk.Button(racine,text='E',command=affichage)
+bouton_e.grid(column=4, row=0)
+
+bouton_f = tk.Button(racine,text='F',command=affichage)
+bouton_f.grid(column=5, row=0)
+
+bouton_g = tk.Button(racine,text='G',command=affichage)
+bouton_g.grid(column=6, row=0)
+
+bouton_h = tk.Button(racine,text='H',command=affichage)
+bouton_h.grid(column=7, row=0)
+
+bouton_i = tk.Button(racine,text='I',command=affichage)
+bouton_i.grid(column=8, row=0)
+
+bouton_j = tk.Button(racine,text='J',command=affichage)
+bouton_j.grid(column=9, row=0)
+
+bouton_k = tk.Button(racine,text='K',command=affichage)
+bouton_k.grid(column=10, row=0)
+
+bouton_l = tk.Button(racine,text='L',command=affichage)
+bouton_l.grid(column=11, row=0)
+
+bouton_m = tk.Button(racine,text='N',command=affichage)
+bouton_m.grid(colum=12, row=0)
+
+bouton_n = tk.Button(racine,text='O',command=affichage)
+bouton_n.grid(colum=0, row=1)
+
+bouton_o = tk.Button(racine,text='P',command=affichage)
+bouton_o.grid(colum=1, row=1)
+
+bouton_a = tk.Button(racine,text='Q',command=affichage)
+bouton_a.grid(colum=2, row=0)
+
+bouton_a = tk.Button(racine,text='Q',command=affichage)
+bouton_a.grid(colum=2, row=0)
 bouton_a = tk.Button(racine,text='M',command=affichage)
-bouton_a.grid(colum=12, row=0)
-bouton_a = tk.Button(racine,text='M',command=affichage)
-bouton_a.grid(colum=12, row=0)
-bouton_a = tk.Button(racine,text='M',command=affichage)
-bouton_a.grid(colum=12, row=0)
-bouton_a = tk.Button(racine,text='M',command=affichage)
-bouton_a.grid(colum=12, row=0)
-bouton_a = tk.Button(racine,text='M',command=affichage)
-bouton_a.grid(colum=12, row=0)
-bouton_a = tk.Button(racine,text='M',command=affichage)
-bouton_a.grid(colum=12, row=0)
-bouton_a = tk.Button(racine,text='M',command=affichage)
-bouton_a.grid(colum=12, row=0)
+bouton_a.grid(colum=, row=0)
 bouton_a = tk.Button(racine,text='M',command=affichage)
 bouton_a.grid(colum=12, row=0)
 bouton_a = tk.Button(racine,text='M',command=affichage)
