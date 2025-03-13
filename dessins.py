739c1adcb3fdd647db124054bce3f398bafd2ce7
irmakaltay <irmakaltay2006@icloud.com>
2025-03-13 17:23:15 +0100

    dessin de l'etape 1

--- dessins.py
+++ dessins.py
@@ -5,12 +5,13 @@ canvas= tk.Canvas(racine,width= 500, height=500)
 
 x0= 100
 y= 100
-x1= 400
+x1= 350
 ligne1_etape1= canvas.create_line(x0, y, x1, y)
-ligne2_etape1=canvas.create_line(x0, y, x0, y- 400)
-ligne3_etape1=canvas.create_line(x0-50, y, x0 + 100,y)
-ligne4_etape1= canvas.create_line(x1, y, x1, y- 30)
-ligne_diagonale_etape1= canvas.create_line(x0 + 20, y, x0, y- 20)
+ligne2_etape1=canvas.create_line(x0, y, x0, y + 300)
+ligne3_etape1=canvas.create_line(x0-10, y, x0 + 100,y)
+ligne4_etape1= canvas.create_line(x1, y, x1, y +30)
+ligne_diagonale_etape1= canvas.create_line(x0 + 20, y, x0, y+ 20)
+ligne_horizontale_etape1= canvas.create_line(x0- 10, y+ 300, x1-150, y+300)
 
 
 canvas.grid(column= 1, row= 3, columnspan= 11, rowspan= 10)
