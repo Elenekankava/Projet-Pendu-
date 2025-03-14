73b96b940f85fe257838ba044ce7a9f2346af355
manon123-design <manon.pinar@gmail.com>
2025-03-14 09:09:13 +0100

    liste de mot

---
+++ mots.py
@@ -0,0 +1,8 @@
+
+Listemots= { "3 lettres": ["coq", "ski", "jus", "nul","gaz", "axe"],
+            "4 lettres": ["beau", "joli", "thym", "yogi", "pion"],
+            "5 lettres" : ["bruit", "cycle", "livre", "pomme", "tenue", "rugby"],
+            "6 lettres" : ["agneau", "alarme", "ananas", "arcade", "billet", "garage", "oiseau"],
+            "7 lettres" : ["puzzle", "abriter", "billard", "chariot", "crapaud", "fourmis", "iceberg"], 
+            "8 lettres" : ["javelot", "losange", "spirale", "aquarium", "brocante", "diapason","objectif", "logiciel", "pastiche", "scorpion", "tabouret", "triangle", "utopique"]
+}
