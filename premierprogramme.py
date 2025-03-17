ef7d644ebf6835ff54266514d412483ecaf0b525
EleneKankava <leni.kankava@gmail.com>
2025-03-17 15:27:44 +0100

    pageaccueildebut

--- premierprogramme.py
+++ premierprogramme.py
@@ -1,5 +1,19 @@
+
 import tkinter as tk
 
 racine= tk.Tk()
 racine.title("jeu du pendu")
-racine.mainloop() 
+
+canvas=tk.Canvas(racine, width=500, height=500)
+canvas.grid()
+
+texte=tk.Label (racine, text="Bienvenue au Jeu du pendu!", font=("Helvetica", "40"),  fg="blue")
+texte.grid(column=0, row=0, padx=10, pady=10)
+
+boutoninstructions=tk.Button (racine, text="Cliquez sur ce bouton afin de lire les instructions du jeu", font=("Helvetica", "20"), bg="pink", fg="black")
+boutoninstructions.grid(column=0, row=1, padx=10, pady=10)
+
+boutondebutjeu= tk.Button(racine, text="Cliquez sur ce bouton afin de commencer a jouer", font=("Helvetica", "20"), bg="pink", fg="black")
+boutondebutjeu.grid(column=0, row=2, padx=10, pady=10)
+
+racine.mainloop()
