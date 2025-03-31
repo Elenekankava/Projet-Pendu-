3a74035c7b0529689f7f62749edf11500fc3c800
manon123-design <manon.pinar@gmail.com>
2025-03-31 15:27:21 +0200

    couleur

--- premierprogramme.py
+++ premierprogramme.py
@@ -3,11 +3,12 @@ import tkinter as tk
 
 racine= tk.Tk()
 racine.title("jeu du pendu")
+racine.configure(bg='white')
 
-canvas=tk.Canvas(racine, width=500, height=500)
+canvas=tk.Canvas(racine, width=500, height=500, bg='white')
 canvas.grid()
 
-texte=tk.Label (racine, text="Bienvenue au Jeu du pendu!", font=("Helvetica", "40"),  fg="blue")
+texte=tk.Label (racine, text="Bienvenue au Jeu du pendu!", font=("Helvetica", "40"),  fg="blue", bg='white')
 texte.grid(column=0, row=0, padx=10, pady=10)
 
 boutoninstructions=tk.Button (racine, text="Cliquez sur ce bouton afin de lire les instructions du jeu", font=("Helvetica", "20"), bg="pink", fg="black")
