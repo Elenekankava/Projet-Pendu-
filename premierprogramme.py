70bd16a892f69f5d6697f0b96b2c705c6d8c6605
EleneKankava <leni.kankava@gmail.com>
2025-03-31 15:46:08 +0200

    pareil

--- premierprogramme.py
+++ premierprogramme.py
@@ -1,30 +1,30 @@
 
 import tkinter as tk
 
+def show_instructions():
+    window=tk.Toplevel(racine)
+
 racine= tk.Tk()
 racine.title("jeu du pendu")
 
-def show_instructions():
-    window=tk.Toplevel(racine)
+
+boutoninstructions=tk.Button (racine, text="Cliquez sur ce bouton afin de lire les instructions du jeu", font=("Helvetica", "20"), bg="pink", fg="black")
+boutoninstructions.grid(column=0, row=1, padx=10, pady=10)
 
 def demarrer():
     texte.destroy()
     boutondebutjeu.destroy()
     boutoninstructions.destroy()
-    
+
 canvas=tk.Canvas(racine, width=500, height=500)
 canvas.grid()
 
 texte=tk.Label (racine, text="Bienvenue au Jeu du pendu!", font=("Helvetica", "40"),  fg="blue")
 texte.grid(column=0, row=0, padx=10, pady=10)
 
-boutoninstructions=tk.Button (racine, text="Cliquez sur ce bouton afin de lire les instructions du jeu", font=("Helvetica", "20"), bg="pink", fg="black", command = show_instructions())
-boutoninstructions.grid(column=0, row=1, padx=10, pady=10)
+
 
 boutondebutjeu= tk.Button(racine, text="Cliquez sur ce bouton afin de commencer a jouer", font=("Helvetica", "20"), bg="pink", fg="black", command=demarrer())
 boutondebutjeu.grid(column=0, row=2, padx=10, pady=10)
-
-
-
 
 racine.mainloop()
