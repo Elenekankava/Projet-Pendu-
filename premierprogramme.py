3327d26915807f42ad83891c2158b6a8759462a8
EleneKankava <leni.kankava@gmail.com>
2025-03-31 15:37:59 +0200

    pageaccueil

--- premierprogramme.py
+++ premierprogramme.py
@@ -3,18 +3,28 @@ import tkinter as tk
 
 racine= tk.Tk()
 racine.title("jeu du pendu")
-racine.configure(bg='white')
 
-canvas=tk.Canvas(racine, width=500, height=500, bg='white')
+def show_instructions():
+    window=tk.Toplevel(racine)
+
+def demarrer():
+    texte.destroy()
+    boutondebutjeu.destroy()
+    boutoninstructions.destroy()
+    
+canvas=tk.Canvas(racine, width=500, height=500)
 canvas.grid()
 
-texte=tk.Label (racine, text="Bienvenue au Jeu du pendu!", font=("Helvetica", "40"),  fg="blue", bg='white')
+texte=tk.Label (racine, text="Bienvenue au Jeu du pendu!", font=("Helvetica", "40"),  fg="blue")
 texte.grid(column=0, row=0, padx=10, pady=10)
 
-boutoninstructions=tk.Button (racine, text="Cliquez sur ce bouton afin de lire les instructions du jeu", font=("Helvetica", "20"), bg="pink", fg="black")
+boutoninstructions=tk.Button (racine, text="Cliquez sur ce bouton afin de lire les instructions du jeu", font=("Helvetica", "20"), bg="pink", fg="black", command = show_instructions())
 boutoninstructions.grid(column=0, row=1, padx=10, pady=10)
 
-boutondebutjeu= tk.Button(racine, text="Cliquez sur ce bouton afin de commencer a jouer", font=("Helvetica", "20"), bg="pink", fg="black")
+boutondebutjeu= tk.Button(racine, text="Cliquez sur ce bouton afin de commencer a jouer", font=("Helvetica", "20"), bg="pink", fg="black", command=demarrer())
 boutondebutjeu.grid(column=0, row=2, padx=10, pady=10)
+
+
+
 
 racine.mainloop()
