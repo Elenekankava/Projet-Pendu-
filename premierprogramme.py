121b5c63c3c9c318ed741415c45e34ef666fa0ea
EleneKankava <leni.kankava@gmail.com>
2025-04-02 20:12:52 +0200

    ameliorationpageaccueil

--- premierprogramme.py
+++ premierprogramme.py
@@ -1,35 +1,33 @@
 
 import tkinter as tk
 
-def show_instructions():
-    window=tk.Toplevel(racine)
-
 racine= tk.Tk()
 racine.title("jeu du pendu")
+racine.geometry("800x600")
 
-t=False
+def clear_window():
+    for widget in racine.winfo_children():
+        widget.destroy()
+
+def page_intruction():
+    clear_window
+    textes=tk.Label(racine, text='instructions', font='30', fg='black')
+
+def deuxieme_page():
+    clear_window
+
+def premiere_page():
+    canvas=tk.Canvas(racine, width=500, height=500)
+    canvas.grid()
+    texte=tk.Label (racine, text="Bienvenue au Jeu du pendu!", font=("Helvetica", "40"),  fg="blue")
+    texte.grid(column=0, row=0, padx=10, pady=10)
+
 
 boutoninstructions=tk.Button (racine, text="Cliquez sur ce bouton afin de lire les instructions du jeu", font=("Helvetica", "20"), bg="pink", fg="black")
 boutoninstructions.grid(column=0, row=1, padx=10, pady=10)
-
-def demarrer():
-    global t
-    t=True
-
-canvas=tk.Canvas(racine, width=500, height=500)
-canvas.grid()
-
-texte=tk.Label (racine, text="Bienvenue au Jeu du pendu!", font=("Helvetica", "40"),  fg="blue")
-texte.grid(column=0, row=0, padx=10, pady=10)
-
-
-
 boutondebutjeu= tk.Button(racine, text="Cliquez sur ce bouton afin de commencer a jouer", font=("Helvetica", "20"), bg="pink", fg="black", command=demarrer())
 boutondebutjeu.grid(column=0, row=2, padx=10, pady=10)
 
-if t==True:
-    boutondebutjeu.destroy()
-    texte.destroy()
-    boutoninstructions.destroy()
+premiere_page()
 
 racine.mainloop()
